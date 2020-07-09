26782a082c267f228752e4bd213df440ea5c19d4
TAKIZAWA Yozo <y@nbk.bz>
2020-07-09 22:28:54 +0900

    Changed: main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def handle_message(e):
     r = e.message.text
     if (r.lower() == 'help'):
         r = '『help』以外の送信メッセージはそのままオウム返しします．'
-    bot.reply_message(e.reply_token, TextSendMessage(text=replyMes))
+    bot.reply_message(e.reply_token, TextSendMessage(text=r))
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
