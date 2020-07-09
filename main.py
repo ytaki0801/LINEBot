06b629f198ff6acd7f9cfdf50937e0d8a098aaff
TAKIZAWA Yozo <y@nbk.bz>
2020-07-09 22:20:39 +0900

    Modified: main.py

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from linebot            import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models     import MessageEvent, TextMessage, TextSendMessage
 
-App = Flask(__name__)
+app = Flask(__name__)
 
-Bot = LineBotApi(os.environ["ACCESS_TOKEN"])
-Handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
+bot = LineBotApi(os.environ["ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
 
-@App.route("/", methods=['POST'])
+@app.route("/", methods=['POST'])
 def callback():
-    body = request.get_data(as_text=True)
     sign = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
     try:
-        Handler.handle(body, sign)
+        handler.handle(body, sign)
     except InvalidSignatureError:
         abort(400)
     return('OK')
 
-@Handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(e):
     replyMes = e.message.text
     replyMes = '『help』以外の送信メッセージそのままオウム返しします．'
-    Bot.reply_message(e.reply_token, TextSendMessage(text=replyMes))
+    bot.reply_message(e.reply_token, TextSendMessage(text=replyMes))
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
