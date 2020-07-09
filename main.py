e4e628cd524d558bc06183e1757e1acba4533579
TAKIZAWA Yozo <y@nbk.bz>
2020-07-09 22:57:07 +0900

    Modified: main.py

--- main.py
+++ main.py
@@ -16,21 +16,19 @@ def callback():
 
 @H.add(MessageEvent, message=TextMessage)
 def handle_message(e):
-    r = e.message.text
-    if (r.lower() == 'help'):
-        r = '『help』以外の送信メッセージはそのままオウム返しします．'
+    u = e.message.text
+    if (u.lower() == 'help'):
+        r = '関東の県名（『茨城』や『東京都』）を送信すると，その県に高専があるかどうかが返信されます．'
     else:
-
         OK = [ '茨城', '茨城県', '栃木', '栃木県', '群馬', '群馬県', '東京', '東京都', '千葉', '千葉県']
         NO = [ '神奈川', '神奈川県', '埼玉', '埼玉県' ]
-        r += 'に高専はありま'
-        if r in OK:
+        r = u + 'に高専はありま'
+        if u in OK:
             r += 'す．'
         elif r in NO:
-            r += 'せん．'
+            u += 'せん．'
         else:
             r = '関東の県名を送信して下さい．'
-
     B.reply_message(e.reply_token, TextSendMessage(text=r))
 
 if __name__ == "__main__":
