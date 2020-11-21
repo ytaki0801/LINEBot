5ab83bc3b28783e772c890dd6c4b2109b829deba
TAKIZAWA Yozo <y@nbk.bz>
2020-11-21 22:01:34 +0900

    Renamed

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ import os
 from flask          import Flask, request
 from linebot        import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import math
 
-HELP = 'Python eval will be executed for input string'
+HELP = '都道府県名（『茨城』や『東京都』）を送信すると，その都道府県に高専があるかどうかが返信されます．'
+OK0 = [ '北海道', '東京', '東京都', '京都', '京都府', '大阪', '大阪府' ]
+OK1 = [ '青森', '岩手', '宮城', '秋田', '山形', '福島' ]
+OK2 = [ '茨城', '栃木', '群馬', '千葉' ]
+OK3 = [ '新潟', '富山', '石川', '福井', '長野', '岐阜', '静岡', '愛知' ]
+OK4 = [ '三重', '兵庫', '奈良', '和歌山' ]
+OK5 = [ '鳥取', '島根', '岡山', '広島', '山口', '徳島', '香川', '愛媛', '高知' ]
+OK6 = [ '福岡', '長崎', '熊本', '大分', '宮崎', '鹿児島', '沖縄' ]
+OK = OK1 + OK2 + OK3 + OK4 + OK5 + OK6
+OK += OK0 + list(map(lambda x: x+'県', OK))
+NO = [ '神奈川', '埼玉', '山梨', '滋賀', '佐賀' ]
+NO += list(map(lambda x: x+'県', NO))
 
 A = Flask(__name__)
 B = LineBotApi(os.environ["ACCESS_TOKEN"])
@@ -24,7 +34,13 @@ def handle_message(e):
     if (u.lower() == 'help'):
         r = HELP
     else:
-        r = eval("str("+u+")")
+        r = u + 'に高専はありま'
+        if u in OK:
+            r += 'す．'
+        elif u in NO:
+            r += 'せん．'
+        else:
+            r = '都道府県名を送信して下さい．'
     B.reply_message(e.reply_token, TextSendMessage(text=r))
 
 if __name__ == "__main__":
